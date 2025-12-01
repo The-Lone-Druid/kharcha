ed2888771c76cb7334dbece595b4bc8fc057c85d
Zahid Shaikh <zahid.786shaikh@gmail.com>
2025-12-01 20:52:14 +0300

    setup: add tanstack libraries, convex and shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
-import App from '../App'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: App,
-})
+export const Route = createFileRoute("/")({
+  component: IndexRoute,
+});
+
+function IndexRoute() {
+  return <div>Index Route</div>;
+}
